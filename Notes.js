eebc6a41ba5d008cb60f37f348ae187cb16522b3
Ely Dorado <2018dorado@gmail.com>
2023-08-14 13:29:09 -0500

    Notes added

---
+++ Notes.js
@@ -0,0 +1,15 @@
+/*
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+This tag allows the webpage to be displayed on mobile devices 
+and adjusts to its width and height
+
+
+
+
+
+
+
+
+*/

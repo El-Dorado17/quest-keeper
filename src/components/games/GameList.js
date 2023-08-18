b7a78e378fc7476f80181b7ce80796d9a0f320f3
Ely Dorado <2018dorado@gmail.com>
2023-08-18 15:49:33 -0500

    2 issues closed

--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -55,7 +55,7 @@ export const GameList=()=> {
     
   return (
     <>
-      <h2 className='text-2xl font-bold mb-4'>Here are {questUserObject.id}'s games:</h2>
+      <h2 className='text-2xl font-bold mb-4'>{questUserObject.name}'s Games:</h2>
       <article className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {cards.map((card, index) => {
           return (
